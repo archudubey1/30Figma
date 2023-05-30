23c229aea2eb6977f79e547c5e9cb750ca95cbf7
Archana dubey <47237986+archudubey1@users.noreply.github.com>
2023-05-30 18:52:51 +0530

    Add files via upload

---
+++ FigmaDesign2/resp.js
@@ -0,0 +1,15 @@
+burger = document.querySelector('.burger')
+mindnavbar  = document.querySelector('.mindnavbar ')
+console.log(burger);
+// rightnavbar = document.querySelector('.rightnavbar')
+// logo = document.querySelector('.logo')
+
+sidemain = document.querySelector('.sidemain')
+
+burger.addEventListener('click', ()=>{
+    // rightnavbar.classList.toggle('v-class-resp');
+    mindnavbar.classList.toggle('v-class-resp');
+    mindnavbar.classList.toggle('h-nav-resp');
+
+})
+console.log(burger);
